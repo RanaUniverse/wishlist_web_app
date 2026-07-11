56c880a569561c1507d5fa948017271a484c0736
RanaUniverse <RanaUniverse321@gmail.com>
2026-07-11 09:44:18 +0530

    now my app can run on gunicorn backend container and then it will run goodly in postgres

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,46 @@
 # gunicorn.conf.py
 
-bind = "0.0.0.0:8001"
+# i will want to take the app_port value form the env
 
-workers = 4
-threads = 1
 
+import multiprocessing
+import os
+
+port_value = os.getenv(key="APP_PORT")
+workers = os.getenv(key="GUNICORN_WORKERS")
+threads = os.getenv(key="GUNICORN_THREADS")
+
+
+DEFAULT_PORT = 9999
+DEFAULT_WORKERS = multiprocessing.cpu_count()
+DEFAULT_THREADS = 2
+
+
+# Port
+port_value = os.getenv("APP_PORT")
+if port_value is None:
+    port_value = DEFAULT_PORT
+    print("APP_PORT not set. Using default: 9999")
+else:
+    port_value = int(port_value)
+
+
+# Workers
+workers = os.getenv("GUNICORN_WORKERS")
+if workers is None:
+    workers = DEFAULT_WORKERS
+    print(f"GUNICORN_WORKERS not set. Using default: {workers}")
+else:
+    workers = int(workers)
+
+
+# Threads
+threads = os.getenv("GUNICORN_THREADS")
+if threads is None:
+    threads = 2
+    print(f"GUNICORN_THREADS not set. Using default: {threads}")
+else:
+    threads = int(threads)
+
+
+bind = f"0.0.0.0:{port_value}"
